567c0412df35adad34869533ae86bf881cac047c
Jason Bisanti <jasonbisanti@gmail.com>
2013-06-23 20:08:16 -0600

    added additional methods

--- Bisanti/src/org/bisanti/util/Util.java
+++ Bisanti/src/org/bisanti/util/Util.java
@@ -5,7 +5,9 @@
 package org.bisanti.util;
 
 import java.util.Collection;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -15,6 +17,26 @@ public final class Util
 {
     private Util(){};
     
+    public static boolean containsNull(Object obj, Object... others)
+    {
+        if(obj == null)
+        {
+            return true;
+        }
+        else if(others != null && others.length > 0)
+        {
+            for(Object other: others)
+            {
+                if(other == null)
+                {
+                    return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+    
     public static boolean equal(Object obj1, Object obj2)
     {
         return obj1 == null ? obj2 == null : obj1.equals(obj2);
@@ -22,7 +44,7 @@ public final class Util
     
     public static boolean equalValues(Number num1, Number num2)
     {
-        if(num1 == null || num2 == null)
+        if(containsNull(num1, num2))
         {
             return num1 == num2;
         }
@@ -40,6 +62,84 @@ public final class Util
     public static boolean isNullOrEmpty(Map map)
     {
         return map == null || map.isEmpty();
+    }
+    
+    public static boolean equalCollections(Collection col1, Collection col2, boolean considerOrder)
+    {
+        if(containsNull(col1, col2))
+        {
+            return col1 == col2;
+        }
+        else if(considerOrder)
+        {
+            if(col1.size() == col2.size())
+            {
+                Iterator it1 = col1.iterator();
+                Iterator it2 = col2.iterator();
+                while(it1.hasNext())
+                {
+                    if(!it1.next().equals(it2.next()))
+                    {
+                        return false;
+                    }
+                }
+                
+                return true;
+            }
+            else
+            {            
+                return false;
+            }
+        }
+        else
+        {
+            return col1.size() == col2.size() && col1.containsAll(col2);
+        }
+    }
+    
+    public static boolean equalMaps(Map map1, Map map2, boolean considerOrder)
+    {
+        if(containsNull(map1, map2))
+        {
+            return map1 == map2;
+        }
+        else if(considerOrder)
+        {
+            if(map1.size() == map2.size())
+            {
+                Iterator<Map.Entry> it1 = map1.entrySet().iterator();
+                Iterator<Map.Entry> it2 = map2.entrySet().iterator();
+                while(it1.hasNext())
+                {
+                    Map.Entry entry1 = it1.next();
+                    Map.Entry entry2 = it2.next();
+                    if(!equal(entry1.getKey(), entry2.getKey()) ||
+                       !equal(entry1.getValue(), entry2.getValue()))
+                    {
+                        return false;
+                    }
+                }
+                
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        else
+        {
+            Set<Map.Entry> entries = map1.entrySet();
+            for(Map.Entry entry: entries)
+            {
+                if(!equal(entry.getValue(), map2.get(entry.getKey())))
+                {
+                    return false;
+                }
+            }
+            
+            return true;
+        }
     }
     
 }
