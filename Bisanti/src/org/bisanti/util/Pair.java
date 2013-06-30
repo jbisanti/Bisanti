f16fa6e38e2235b75d82835708b06b391c71ac02
Jason Bisanti <jasonbisanti@gmail.com>
2013-06-30 08:25:19 -0600

    added Pair class

---
+++ Bisanti/src/org/bisanti/util/Pair.java
@@ -0,0 +1,34 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.bisanti.util;
+
+/**
+ * Written and authored by Jason Bisanti. Free to use and reproduce.
+ *
+ * @author Jason Bisanti
+ */
+public class Pair<T, V>
+{
+    private T first;
+    
+    private V second;
+
+    public Pair(T first, V second)
+    {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst()
+    {
+        return first;
+    }
+
+    public V getSecond()
+    {
+        return second;
+    }
+    
+}
